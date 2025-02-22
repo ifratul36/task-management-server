b16c1d0a31a5da25d9a28762388f1936d5cd7352
ifratul36 <jannatrituwd@gmail.com>
2025-02-22 16:55:19 +0600

    user get route

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ async function run() {
 
       // app.delete("/tasks/:id",async (req, res) => {
       //   const id = req.params.id;
-      //   const query = { _id: new ObjectId(id) };
 
       // PUT - Update a task by ID
 app.put("/tasks/:id", async (req, res) => {
